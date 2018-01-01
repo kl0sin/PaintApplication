9ce6e27cf5d484cc51f62480030f47b9b6ffe780
Mateusz Kłosiński <mkklosin@gmail.com>
2018-01-01 23:37:56 +0100

    Revert

--- scripts/savingPhoto.js
+++
@@ -1,8 +0,0 @@
-const savePhotoButton = document.querySelector('.save-photo');
-savePhotoButton.addEventListener('click', savePhoto);
-
-function savePhoto() {
-    const data = canvas.toDataURL('image/jpeg');
-    savePhotoButton.setAttribute('download', 'my-picture');
-    savePhotoButton.href = data;
-}
